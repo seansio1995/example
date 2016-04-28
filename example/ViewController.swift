35b80d82d6fb2f84fcfff3f1ba40005814568458
seansio1995 <seansio@163.com>
2016-04-28 16:01:40 +0800

    final commit

--- example/ViewController.swift
+++ example/ViewController.swift
@@ -11,11 +11,15 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var myLabel: UILabel!
 
+    @IBAction func buttonPressed(sender: AnyObject) {
+        
+        myLabel.text="It worked"
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
        // println("Hello World")
-        myLabel.text="It worked"
+        
         
     }
 
